98bd605c2c2c53fa7c1859994e7b6dbf2f24fa0a
quanghaitb <nguyenquanghai2331998@gmail.com>
2020-07-01 11:33:55 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-app>
+    <v-app-bar app
+      _absolute
+      color="dark"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/900/300?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-target"
+      scroll-threshold="500"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(65, 120, 163,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+      
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <v-avatar _size="48" class="mr-2">
+        <img src="https://material.io/resources/icons/static/ic_material_192px_light.svg">
+      </v-avatar>
+      
+      <v-toolbar-title class="text-h6 text-sm-h5">Material Design Showcase</v-toolbar-title>
+
+      <v-spacer/>
+
+      <v-btn
+        href="https://materialdesignkit.com"
+        target="_blank"
+        text
+        class="hidden-xs-only">
+        <span class="mr-2">Buy</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main id="scrolling-target" class="grey lighten-4">
+      <v-container fluid>
+        <v-row>
+          <v-col v-for="n in templates.length" :key="n" cols="12" sm="6" md="4" lg="3">
+            <v-skeleton-loader
+              :loading="skeletonLoading"
+              transition="scale-transition"
+              class="mx-auto"
+              max-width="300"
+              type="card">
+              <ShowCase :templateItem="templates[n-1]"/>
+            </v-skeleton-loader>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer app class="grey darken-3 white--text">
+      <div class="ml-3 text-caption text-sm-body-2">
+          Made with <v-icon class="red--text" style="vertical-align: middle;" _large>mdi-heart-outline</v-icon>
+          by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+          and <a class="white--text" href="https://github.com/hoangtuan151" target="_blank">Author</a>
+      </div>
+      <v-spacer/>
+      <div class="mr-3 hidden-xs-only">
+          Copyright &copy; 2020
+      </div>
+    </v-footer>
+
+  </v-app>
+</template>
+
+<script>
+import ShowCase from '@/components/ShowCase';
+import axios from 'axios'
+
+export default {
+  name: 'App',
+
+  components: {
+    ShowCase,
+  },
+
+  data: () => ({
+    skeletonLoading: true,
+    templates: []
+  }),
+
+  async created () {
+    setTimeout(() => {
+      this.skeletonLoading = false
+    }, 1500);
+
+    let resp = await axios.get('/material-app-design-showcase/data.json')
+    if (resp.data) {
+      this.templates = resp.data
+    }
+  }
+};
+</script>
---
+++ src/components/MobileSlideShow.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-dialog v-model="modaldialog" @input="$root.$emit('SLIDE_SHOW_CLOSED')" >
+    <v-sheet
+      class="mx-auto"
+      elevation="2"
+    >
+      <div style="position: absolute; ">
+        <v-icon class="" x-large @click="closeMobileSlideShow" fixed="true" color="#D50000" >mdi-alpha-x</v-icon>
+      </div>
+      
+      <v-row justify="center">
+        <v-col v-for="n in images.length" :key="n" cols="8" md="5">
+          <v-skeleton-loader
+          :loading="skeletonLoading"
+              transition="scale-transition"
+              class="mx-auto"
+              max-width="300"
+              type="card">
+            <div class="overline text-center font-weight-medium">-- {{ n >= 10 ? n : '0'+n }} --</div>
+          <v-img
+            :src="images[n-1]"
+            _class="ma-2"
+            _height="500" _width="300" contain/>
+          </v-skeleton-loader> 
+        </v-col>
+        <!-- <v-btn class="mx-3" @click="closeMobileSlideShow" fixed="true" right="true" >
+          <v-icon large>mdi-alpha-x</v-icon>
+        </v-btn> -->
+      </v-row>
+    </v-sheet>
+  </v-dialog>
+</template>
+
+<script>
+
+export default {
+  name: 'MobileSlideShow',
+
+
+
+  data: () => ({
+    modaldialog: true
+  }),
+  props: ['images'],
+
+  methods: {
+    closeMobileSlideShow () {
+        this.modaldialog = false
+        setTimeout(() => this.modaldialog = "true", 500)
+    }
+
+    
+  }
+}
+</script>
